fb990f0b71e8821bac8924dd592fab6c9e192376
Bogdan Ionita <bogdan.ionita@stream-financial.com>
2021-12-13 18:29:50 +0000

    Cleaning

--- announcement.py
+++ announcement.py
@@ -18,27 +18,9 @@ class Announcement:
         hours = minutes / 60
         days = hours / 24
 
-        frequency = f'{self.sleep} seconds' if minutes < 1 else (f'{minutes} minutes' if hours < 1  else (f'{hours} hours' if days < 1 else f'{days} days' ))
+        frequency = f'{self.sleep} seconds' if minutes < 1 else (
+            f'{minutes} minutes' if hours < 1 else (f'{hours} hours' if days < 1 else f'{days} days'))
 
-        return f'\'**{self.content[0:20]}...**\' requested by {self.requester}. ' \
-               f'Announces on channel **{self.channel}** every **{frequency}**, until {self.until.strftime("%d/%m/%Y, %H:%M:%S")}. Id: {self.uuid}. '
-
-    # def stop(self):
-    #     self.running = False
-    #     self.thread.stop()
-    #
-    # async def start(self):
-    #     self.thread = threading.Thread(target=asyncio.run, args=(await self.announce(),))
-    #     self.thread.start()
-    #
-    # async def announce(self):
-    #     self.running = True
-    #     passed = None
-    #
-    #     if self.until is None:
-    #         passed = datetime.datetime.now() + datetime.timedelta(days=1000)
-    #
-    #     while datetime.datetime.now() < self.until and self.running is True:
-    #         await self.channel.send(self.content)
-    #         time.sleep(self.sleep)
-
+        return f'"**{self.content[0:20]}...**" requested by {self.requester}. ' \
+               f'Announces on channel **{self.channel}** every **{frequency}**, ' \
+               f'until {self.until.strftime("%d/%m/%Y, %H:%M:%S")}. Id: {self.uuid}. '
--- main.py
+++ main.py
@@ -1,14 +1,12 @@
-import time
-
-from dateutil.parser import parse
 from decouple import config
 from discord_client import DiscordClient
-from announcement import Announcement
-from announcements import Announcements
 
-client = DiscordClient()
-token = config('discordBotToken')
-client.run(token)
+if __name__ == '__main__':
+    client = DiscordClient()
+    token = config('discordBotToken')
+    client.run(token)
+
+
 
 
 
